684cca7f69ea6a6ef3ae68ffa16dbe619b91b9c8
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-09-02 05:44:17 +0600

    feat: add JobListing search filter UI

---
+++ client/src/Components/JobListing.jsx
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
+
+const JobListing = () => {
+  const { isSearched, searchFilter, setSearchFilter } = useContext(AppContext);
+  return (
+    <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
+      {/* Sidebar */}
+      <div className="w-full bg-white px-4">
+        {/* Search Filter from Hero Component */}
+        {isSearched &&
+          (searchFilter.title !== "" || searchFilter.location !== "") && (
+            <>
+              <h3 className="font-medium text-lg mb-4">Current Search</h3>
+              <div className="mb-4 text-gray-600 ">
+                {searchFilter.title && (
+                  <span className="inline-flex items-center gap-2.5 border  bg-blue-50 border-blue-200 px-4 py-1.5 rounded">
+                    {searchFilter.title}
+                    <img
+                      onClick={() =>
+                        setSearchFilter((prev) => ({ ...prev, title: "" }))
+                      }
+                      className="cursor-pointer"
+                      src={assets.cross_icon}
+                    />
+                  </span>
+                )}
+                {searchFilter.location && (
+                  <span className="ml-5 inline-flex items-center gap-2.5 border  bg-red-50 border-red-200 px-4 py-1.5 rounded">
+                    {searchFilter.location}
+                    <img
+                      onClick={() =>
+                        setSearchFilter((prev) => ({ ...prev, location: "" }))
+                      }
+                      className="cursor-pointer"
+                      src={assets.cross_icon}
+                    />
+                  </span>
+                )}
+              </div>
+            </>
+          )}
+      </div>
+    </div>
+  );
+};
+
+export default JobListing;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
+import JobListing from '../Components/JobListing'
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Hero/>
+        <JobListing/>
     </div>
   )
 }
