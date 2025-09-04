f9ed4f63fd6b74a9eb6c1e3d72e4120cd2813d4c
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-09-05 01:28:22 +0600

    appdownload done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Hero from '../Components/Hero'
 import JobListing from '../Components/JobListing'
+import AppDownload from '../Components/AppDownload'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Navbar/>
         <Hero/>
         <JobListing/>
+        <AppDownload/>
     </div>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
-    port: 5174,
+    port: 5175,
   },
 });
