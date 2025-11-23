64722b84a2e9736b25665a75e25e2a972015592d
naimsheikh8020 <naimsheikh8020@gmail.com>
2025-11-24 04:16:45 +0600

    View Application added

--- client/src/pages/ViewApplication.jsx
+++ client/src/pages/ViewApplication.jsx
@@ -1,8 +1,82 @@
 import React from 'react'
+import { assets, viewApplicationsPageData } from '../assets/assets'
 
 const ViewApplication = () => {
   return (
-    <div>ViewApplication</div>
+    <div className='container mx-auto p-4 '>
+      <div>
+        <table className='w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm'>
+          <thead>
+            <tr>
+              <th className='py-2 px-4 text-left'>#</th>
+              <th className='py-2 px-4 text-left'>User Name</th>
+              <th className='py-2 px-4 text-left max-sm:hidden'>Job Title</th>
+              <th className='py-2 px-4 text-left max-sm:hidden'>Location</th>
+              <th className='py-2 px-4 text-left'>Resume</th>
+              <th className='py-2 px-4 text-left'>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {viewApplicationsPageData.map((applicant, index) => (
+              <tr key={index}>
+                <td className='py-2 px-4 border-b border-gray-300 text-center'>
+                  {index + 1}
+                </td>
+
+                {/* IMAGE + NAME SAME ROW */}
+                <td className='py-2 px-4 border-b border-gray-300'>
+                  <div className="flex items-center gap-2">
+                    <img
+                      className='w-10 h-10 rounded-full max-sm:hidden'
+                      src={applicant.imgSrc}
+                      alt={applicant.name}
+                    />
+                    <span>{applicant.name}</span>
+                  </div>
+                </td>
+
+                <td className='py-2 px-4 border-b border-gray-300 max-sm:hidden'>
+                  {applicant.jobTitle}
+                </td>
+
+                <td className='py-2 px-4 border-b border-gray-300 max-sm:hidden'>
+                  {applicant.location}
+                </td>
+
+                <td className='py-2 px-4 border-b border-gray-300'>
+                  <a
+                    href=''
+                    target='_blank'
+                    className='bg-blue-50 text-blue-400 px-3 py-1 rounded inline-flex gap-2 items-center'
+                  >
+                    Resume
+                    <img src={assets.resume_download_icon} />
+                  </a>
+                </td>
+
+                {/* ACTION MENU */}
+                <td className='py-2 px-4 border-b border-gray-300 relative'>
+                  <div className='relative inline-block text-left group'>
+                    <button className='text-gray-500 action-button'>...</button>
+
+                    <div className='z-10 hidden absolute right-0 md:left-0 top-0 mt-2 w-32 bg-white border border-gray-200 rounded shadow group-hover:block'>
+                      <button className='block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100'>
+                        Accept
+                      </button>
+                      <button className='block w-full text-left px-4 py-2 text-red-500 hover:bg-gray-100'>
+                        Reject
+                      </button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+
+        </table>
+      </div>
+    </div>
   )
 }
 
